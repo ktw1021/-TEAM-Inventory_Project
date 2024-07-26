df13fe2bc3a61de451aeb92ea0209ef24689cf03
ebiten-gije <gije08@gmail.com>
2024-07-26 10:59:00 +0900

    로그인 실패 문구 수정

--- src/main/webapp/javascript/users.js
+++ src/main/webapp/javascript/users.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const urlParams = new URLSearchParams(window.location.search);
     const errorParam = urlParams.get('error');
     if (errorParam) {
-        alert(decodeURIComponent(errorParam));
+        alert('아이디 또는 비밀번호가 잘못 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요.');
     }
 });
 
